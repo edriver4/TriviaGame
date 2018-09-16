b8d5914c6a2fc656dcea282e0b27b652429a2cf0
Deezy <brad.driver33@gmail.com>
2018-09-16 19:14:46 -0400

    made div with id = quiz to control when the quiz displays

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//How can I control when the game starts without displaying the questions?
+// Game starts when start button is clicked
+// Timer starts 
+    //Timer has 1 minute and 20 seconds
+    //Once timer is done the game is over
+        //Displays the answers that are correct and incorrect
+    //If player submits their answers before the time is up the player can click submit button
+        //Displays the answers that are correct and incorrect
+//
