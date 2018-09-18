6e8c1418c54d5cd9baafc2c275d5dea8458d6a11
Deezy <brad.driver33@gmail.com>
2018-09-18 11:40:04 -0400

    Fixed the typo in the start function 'length' and then called the start function in the run function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,50 +50,7 @@ var questions = [
         answers: [ 'Australia', 'New Zealand', 'Scottland', 'England'],
         correctAnswer: 'New Zealand'
     }
-];
-
-// var questions = {
-//     q1: {
-//         name: "question 1",
-//         choices: [$("#q1Incorrect1"), $("#q1Incorrect2"), $("#q1Incorrect3"), $("#q1Correct")],
-//         answer: $("#q1Correct"),
-//     },
-//     q2: {
-//         name: "question 2",
-//         choices: [$("#q2Incorrect1"), $("#q2Incorrect2"), $("#q2Incorrect3"), $("#q2Correct")],
-//         answer: $("#q2Correct"),
-//     },
-//     q3: {
-//         name: "question 3",
-//         choices: [$("#q3Incorrect1"), $("#q3Incorrect2"), $("#q3Incorrect3"), $("#q3Correct")],
-//         answer: $("#q3Correct"),
-//     },
-//     q4: {
-//         name: "question 4",
-//         choices: [$("#q4Incorrect1"), $("#q4Incorrect2"), $("#q4Incorrect3"), $("#q4Correct")],
-//         answer: $("#q4Correct"),
-//     },
-//     q5: {
-//         name: "question 5",
-//         choices: [$("#q5Incorrect1"), $("#q5Incorrect2"), $("#q5Incorrect3"), $("#q5Correct")],
-//         answer: $("#q5Correct"),
-//     },
-//     q6: {
-//         name: "question 6",
-//         choices: [$("#q6Incorrect1"), $("#q6Incorrect2"), $("#q6Incorrect3"), $("#q6Correct")],
-//         answer: $("#q6Correct"),
-//     },
-//     q7: {
-//         name: "question 7",
-//         choices: [$("#q7Incorrect1"), $("#q7Incorrect2"), $("#q7Incorrect3"), $("#q7Correct")],
-//         answer: $("#q7Correct"),
-//     },
-//     q8: {
-//         name: "question 8",
-//         choices: [$("#q8Incorrect1"), $("#q8Incorrect2"), $("#q8Incorrect3"), $("#q8Correct")],
-//         answer: $("#q8Correct"),
-//     }
-// }   
+]; 
 
 
 // Setting the startTime counter to 90
@@ -103,12 +60,7 @@ var startTime = 90;
 var intervalId;
 
 var usersChoice = $()
-                                // hide();
-                                // function hide () {
-                                    // document.getElementById("quiz").style.display = "none";
-                                // }
-// .hide() function hides all of the content within the "quiz" div.  .hide() is calling the hide function that is predefined in JQuery.
-$("#quiz").hide();
+
 // When the start button gets clicked, run the start function.
 $("#start").on("click", run);
 console.log(run);
@@ -119,13 +71,12 @@ var select = document.getElementsByTagName("input")
 //  The run function sets an interval
 //  that runs the decrement function once a second.
 //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
-//  The .show() function shows the content for the "Quiz" Div.
 function run() {
     console.log(run);
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    $("#quiz").show();
-    
+    // $("#quiz").show();
+    start();
 
     // loop through the questions object and 
 
@@ -133,16 +84,16 @@ function run() {
 
 function start () {
     var quizBox = $("#quiz")
-    for (var i = 0; i < questions.lenght; i++){
-        quizBox.append("<h3>" + questions[i].question + "</h3>")
+    for (var i = 0; i < questions.length; i++){
+        quizBox.append("<h3>" + questions[i].question + "</h3>");
         for (var j = 0; j < questions[i].answers.length; j++){
-            quizBox.append("<input type='radio' name='question-" + i + " ' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j])
+            quizBox.append("<input type='radio' name='question-" + i + " ' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
         }
     }
 }
 
 function done () {
-    $.each($("input[name='question-0]:checked"), function() {
+    $.each($("input[name='question-0']:checked"), function() {
         if($(this).val() === questions[0].correctAnswer){
             correct++
         } else {
@@ -150,14 +101,14 @@ function done () {
         }  
     })
 
-    $.each($("input[name='question-1]:checked"), function() {
+    $.each($("input[name='question-1']:checked"), function() {
         if($(this).val() === questions[1].correctAnswer){
             correct++
         } else {
             incorrect++ 
     }
     })
-    $.each($("input[name='question-2]:checked"), function() {
+    $.each($("input[name='question-2']:checked"), function() {
         if($(this).val() === questions[2].correctAnswer){
             correct++
         } else {
@@ -165,7 +116,7 @@ function done () {
         }
     })
 
-    $.each($("input[name='question-3]:checked"), function() {
+    $.each($("input[name='question-3']:checked"), function() {
         if($(this).val() === question[3].correctAnswer){
             correct++
         } else {
@@ -173,7 +124,7 @@ function done () {
         }
     })
 
-    $.each($("input[name='question-4]:checked"), function() {
+    $.each($("input[name='question-4']:checked"), function() {
         if($(this).val() === question[4].correctAnswer){
             correct++
         } else {
@@ -181,7 +132,7 @@ function done () {
         }
     })
 
-    $.each($("input[name='question-5]:checked"), function() {
+    $.each($("input[name='question-5']:checked"), function() {
         if($(this).val() === question[5].correctAnswer){
             correct++
         } else {
@@ -189,7 +140,7 @@ function done () {
         }
     })
 
-    $.each($("input[name='question-6]:checked"), function() {
+    $.each($("input[name='question-6']:checked"), function() {
         if($(this).val() === question[6].correctAnswer){
             correct++
         } else {
@@ -197,7 +148,7 @@ function done () {
         }
     })
 
-    $.each($("input[name='question-7]:checked"), function() {
+    $.each($("input[name='question-7']:checked"), function() {
         if($(this).val() === question[7].correctAnswer){
             correct++
         } else {
@@ -217,6 +168,7 @@ function decrement() {
         stop();
         // Alert the user that the time is up.
         alert("Your time is up!");
+        done();
     }
 
 }
